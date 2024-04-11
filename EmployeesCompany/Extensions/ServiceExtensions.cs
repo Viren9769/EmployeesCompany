53917e77806f4ab16af7f4c96ff5d1c47377d0de
Viren9769 <133132357+Viren9769@users.noreply.github.com>
2024-04-11 10:00:57 -0500

    Add project files.

---
+++ EmployeesCompany/Extensions/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace EmployeesCompany.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        //  we are going to da is configure CORS in our application 
+        // We are using basic CORS policy settings because allowing any origin, 
+       // method, and header is okay for now.But we should be more
+       // restrictive with those settings in the production environment.More
+       // precisely, as restrictive as possible.
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader());
+        });
+
+        //ASP.NET Core applications are by default self-hosted, and if we want to 
+        //host our application on IIS, we need to configure an IIS integration which
+        //will eventually help us with the deployment to IIS.To do that, we need to
+        //add the following code to the ServiceExtensions class:
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+             services.Configure<IISOptions>(options =>
+             {
+             });
+
+    }
+}
---
+++ EmployeesCompany/Program.cs
@@ -0,0 +1,43 @@
+using EmployeesCompany.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//modify it to support CORS and IIS integration 
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// we've added CORS and IIS configuarion to yhe section where we need to configue our service.
+
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
